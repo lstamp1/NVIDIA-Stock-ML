3c8f2aaf4d73c73220da3b31fbf3f25c7daa150b
Liam <liam.stamp@gmail.com>
2024-10-05 21:18:47 -0400

    Added % Daily Move to df_gen

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
 # Main source file to perform ML on Nvidia Stock Price Dataset
 # https://www.kaggle.com/datasets/syedfaizanalii/nividia-stock-dataset-2023-2024
 
+# Objective: 
+# Given: a day's open, high, low, close, Adj close, and volume, 
+# Determine: next day's close using machine learning
+
+
+########################### SETUP #####################################
 # Import Libs
 import pandas as pd
 import numpy as np
@@ -10,6 +16,10 @@ from matplotlib.ticker import MaxNLocator
 # Generate the DataFrame from CSV Data
 df = pd.read_csv("nvidia_stock_data.csv")
 print(f"The df structure looks like... \n {df}")
+
+# Create a second DF that will hold the new data analysis
+df_gen = df
+
 
 # Break out Columns
 dates      = df['Date']
@@ -21,6 +31,7 @@ adj_closes = df['Adj Close']
 volume     = df['Volume']
 
 
+########################### COLUMN GEN ##################################
 
 # Analysis on Daily Moves
 day_results = []
@@ -29,14 +40,23 @@ for i,j in zip(opens, closes):
         day_results.append('Higher') # Ended Higher
     else:
         day_results.append('Lower') # Ended Lower
-# Plot Daily Moves
-plt.hist(day_results, bins=2, edgecolor='black')  
-plt.title('Higher or Lower for Day')
-plt.xlabel('Value')
-plt.ylabel('Frequency')
-plt.show()
+df_gen['Daily Moves'] = day_results # Add column to new DF
+
+# Create column for daily moves ($)
+daily_moves_value = []
+for i,j in zip(opens, closes):
+    daily_moves_value.append(j-i)
+df_gen["Daily Moves ($)"] = daily_moves_value
+
+# Create column for daily moves (%)
+daily_moves_percent = []
+for i,j in zip(opens, closes):
+    daily_moves_percent.append((j-i)/i)
+df_gen["Daily Moves (%)"] = daily_moves_percent
 
 
+
+########################### ANALYSIS ##################################
 
 # Generate Plot
 df.plot.scatter(x = 'Date', y = 'Close')
@@ -47,4 +67,4 @@ plt.gca().xaxis.set_major_locator(MaxNLocator(5))
 plt.grid()
 plt.show()
 #plt.plot()
-
+print(f"New dataset = \n {df_gen}")
